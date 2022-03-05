1da3e4fd1a4c8e1ea62076031692b3be47e4fcce
Steven Enamakel <enamakel@mahadao.com>
2022-03-05 21:04:13 +0000

    added wrapper contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
+// const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const constructorArguments = [""];
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const ArthUSDWrapper = await ethers.getContractFactory("ArthUSDWrapper");
+  const instance = await ArthUSDWrapper.deploy(
+    String(constructorArguments[0]),
+    String(constructorArguments[1]),
+    String(constructorArguments[2]),
+    String(constructorArguments[3]),
+    String(constructorArguments[4]),
+    constructorArguments[5]
+  );
 
-  await greeter.deployed();
+  await instance.deployed();
+  console.log("ArthUSDWrapper deployed to:", instance.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+  await hre.run("verify:verify", {
+    address: instance.address,
+    constructorArguments,
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
