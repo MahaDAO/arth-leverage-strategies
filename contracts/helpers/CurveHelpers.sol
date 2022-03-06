4ff8c95130f7963be389dd6b55a045bf13a4fde8
Steven Enamakel <enamakel@mahadao.com>
2022-03-06 13:38:45 +0000

    added more helprs

---
+++ contracts/helpers/CurveHelpers.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {ILeverageStrategy} from "../interfaces/ILeverageStrategy.sol";
+import {IFlashLoan} from "../interfaces/IFlashLoan.sol";
+import {IFlashBorrower} from "../interfaces/IFlashBorrower.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+abstract contract CurveHelpers {
+    using SafeMath for uint256;
+}
--- contracts/leverage/WMaticExposure.sol
+++ contracts/leverage/WMaticExposure.sol
@@ -2,9 +2,13 @@
 
 pragma solidity ^0.8.0;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import {ILeverageStrategy} from "../interfaces/ILeverageStrategy.sol";
 import {IFlashLoan} from "../interfaces/IFlashLoan.sol";
+import {IUniswapV2Router02} from "../interfaces/IUniswapV2Router02.sol";
 import {TroveHelpers} from "../helpers/TroveHelpers.sol";
+
 import {IFlashBorrower} from "../interfaces/IFlashBorrower.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
@@ -13,21 +17,32 @@ contract WMaticExposure is ILeverageStrategy, IFlashBorrower, TroveHelpers {
 
     event OpenPosition(uint256 amount, address who);
 
-    IFlashLoan public flashLoan;
     address public borrowerOperations;
+    IERC20 public immutable arth;
+    IERC20 public immutable usdc;
+    IERC20 public immutable wmatic;
+    IFlashLoan public flashLoan;
+    IUniswapV2Router02 public immutable uniswapRouter;
 
-    //     IUniswapV2Router02 public uniswapRouter;
-    //     IBorrowerOperations public borrowerOperations;
-
-    constructor(address _flashloan, address _borrowerOperations) {
+    constructor(
+        address _flashloan,
+        address _arth,
+        address _wmatic,
+        address _usdc,
+        address _uniswapRouter,
+        address _borrowerOperations
+    ) {
         flashLoan = IFlashLoan(_flashloan);
+
+        arth = IERC20(_arth);
+        usdc = IERC20(_usdc);
+        wmatic = IERC20(_wmatic);
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
         borrowerOperations = _borrowerOperations;
     }
 
-    function openPosition(uint256 amountIn, uint256 borrowAmount) external override returns (bool) {
-        emit OpenPosition(amountIn + borrowAmount, msg.sender);
+    function openPosition(uint256 amountIn, uint256 borrowAmount) external override {
         flashLoan.flashLoan(address(this), borrowAmount, "o");
-        return true;
     }
 
     function closePosition() external override {
@@ -36,64 +51,111 @@ contract WMaticExposure is ILeverageStrategy, IFlashBorrower, TroveHelpers {
 
     function calculateSlippage(uint256 amountIn, uint256 borrowAmount) external override {
         // nothing
-        emit OpenPosition(amountIn + borrowAmount, msg.sender);
     }
 
     function onFlashLoan(
         address initiator,
-        uint256 amount,
+        uint256 flashloanedAmount,
         uint256 fee,
         bytes calldata data
     ) external override returns (bytes32) {
-        require(msg.sender == address(flashLoan), "Untrusted lender");
+        require(msg.sender == address(flashLoan), "untrusted lender");
+        require(initiator == address(this), "not contract");
 
         // uint256 paybackAmount = amount.add(fee);
-        // (
-        //     uint256 _maxFee,
-        //     uint256 _LUSDAmount,
-        //     uint256 _ETHAmount,
-        //     address _upperHint,
-        //     address _lowerHint,
-        //     address _frontEndTag
-        // ) = abi.decode(
-        //     data,
-        //     (uint256,uint256,uint256,address,address,address)
-        // );
+        (
+            uint256 action,
+            uint256 maxFee,
+            uint256 debt,
+            uint256 collateralAmount,
+            address upperHint,
+            address lowerHint,
+            address frontEndTag
+        ) = abi.decode(data, (uint256, uint256, uint256, uint256, address, address, address));
 
-        // uint256 arthToSwap = amount.div(2);
-        // uint256 token0Out = _swapARTHForToken(arthToToken0Path, arthToSwap);
-        // uint256 token1Out = _swapARTHForToken(arthToToken1Path, arthToSwap);
-        // uint256 liquidityOut = _addLiquidity(token0Out, token1Out);
-
-        // IERC20(pair).transferFrom(initiator, address(this), _ETHAmount.sub(liquidityOut));
-        // IERC20(pair).approve(address(borrowerOperations), _ETHAmount);
-
-        // // 3. Borrow ARTH.
-        // borrowerOperations.openTrove(
-        //     _maxFee,
-        //     _LUSDAmount,
-        //     _ETHAmount,
-        //     _upperHint,
-        //     _lowerHint,
-        //     _frontEndTag
-        // );
-        // require(
-        //     IERC20(arth).balanceOf(address(this)) >= paybackAmount,
-        //     "Wrong payback amount"
-        // );
-        // IERC20(arth).approve(address(flashLoan), paybackAmount);
-
-        onFlashloanOpenPosition();
+        if (action == 0) {
+            onFlashloanOpenPosition(
+                flashloanedAmount,
+                maxFee,
+                debt,
+                collateralAmount,
+                upperHint,
+                lowerHint,
+                frontEndTag
+            );
+        } else onFlashloanClosePosition();
 
         return keccak256("FlashMinter.onFlashLoan");
     }
 
-    function onFlashloanOpenPosition() internal {}
+    function onFlashloanOpenPosition(
+        uint256 flashloanedAmount,
+        uint256 maxFee,
+        uint256 debt,
+        uint256 collateralAmount,
+        address upperHint,
+        address lowerHint,
+        address frontEndTag
+    ) internal {
+        sellARTH(flashloanedAmount, collateralAmount);
+
+        openLoan(
+            borrowerOperations,
+            maxFee,
+            debt,
+            collateralAmount,
+            upperHint,
+            lowerHint,
+            frontEndTag
+        );
+    }
 
     function onFlashloanClosePosition() internal {
-        // 1. use the flashloan to payback the debt
+        // 1. use the flashloan'd ARTH to payback the debt
         closeLoan(borrowerOperations);
 
         // 2. get the collateral and swap back to arth
+        uint256 collateralAmount = wmatic.balanceOf(msg.sender);
+        buyARTH(collateralAmount, 0);
+
+        // 3. payback the loan..
+    }
+
+    function sellARTH(uint256 _arthAmount, uint256 _minSwapAmount) internal returns (uint256) {
+        arth.approve(address(uniswapRouter), _arthAmount);
+
+        address[] memory path = new address[](3);
+        path[0] = address(arth);
+        path[1] = address(usdc);
+        path[2] = address(wmatic);
+
+        uint256[] memory amountsOut = uniswapRouter.swapExactTokensForTokens(
+            _arthAmount,
+            _minSwapAmount,
+            path,
+            address(this),
+            block.timestamp
+        );
+
+        return amountsOut[amountsOut.length - 1];
+    }
+
+    function buyARTH(uint256 _collateralAmount, uint256 _minSwapAmount) internal returns (uint256) {
+        wmatic.approve(address(uniswapRouter), _collateralAmount);
+
+        address[] memory path = new address[](3);
+        path[0] = address(wmatic);
+        path[1] = address(usdc);
+        path[2] = address(arth);
+
+        uint256[] memory amountsOut = uniswapRouter.swapExactTokensForTokens(
+            _collateralAmount,
+            _minSwapAmount,
+            path,
+            address(this),
+            block.timestamp
+        );
+
+        return amountsOut[amountsOut.length - 1];
     }
 }
