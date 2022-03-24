f6bb6f978a067a4881c128cd17dcaaf4bfc3895a
Steven Enamakel <enamakel@mahadao.com>
2022-03-24 11:04:41 +0000

    update script

--- scripts/strategy/lp-staking-exposure/deploy.ts
+++ scripts/strategy/lp-staking-exposure/deploy.ts
@@ -15,7 +15,8 @@ async function main() {
     "0x682D628626136B0D245B71a585E32AEb922D4C1b", // address _borrowerOperations
     "0x90054AB3305E2ab1b6Bd48a9E2F5Ef261b2307f9", // address _wrapper
     "0xB70Ef37fF7eA122D618E50dB8c02c14362680669", // address _proxyRegistry
-    "0xb6310276fD430CF2d19B182cCf95407463C8c484" // address _troveManager
+    "0xb6310276fD430CF2d19B182cCf95407463C8c484", // address _troveManager
+    "0xA873485640D00D76ADc118E40354159676C62F78" // address _priceFeed
   );
 
   await instance.deployed();
@@ -28,14 +29,15 @@ async function main() {
     constructorArguments: [
       "0x7b009aE84341d1C1B64eFEceE2d41273E146dBE6", // address _flashloan,
       "0x8BFE2131a7Cb2072269f53624fd38EaCA6543309", // address _arth,
-      "0x84d5dd10115BcDEeCB644FCdF2fCA63bb00DF92d",
+      "0x7024f0BD884dd335aba9E7C797d47fbC9ee9f944", // address _controller,
       "0x3467D9Fea78e9D82728aa6C3011F881ad7300a1e", // address _maha,
       "0x54406a69B4c629E4d5711140Faec3221672c71A1", // address _dai,
       "0x8954afa98594b838bda56fe4c12a09d7739d179b", // address _uniswapRouter,
       "0x682D628626136B0D245B71a585E32AEb922D4C1b", // address _borrowerOperations
       "0x90054AB3305E2ab1b6Bd48a9E2F5Ef261b2307f9", // address _wrapper
       "0xB70Ef37fF7eA122D618E50dB8c02c14362680669", // address _proxyRegistry
-      "0xb6310276fD430CF2d19B182cCf95407463C8c484" // address _troveManager
+      "0xb6310276fD430CF2d19B182cCf95407463C8c484", // address _troveManager
+      "0xA873485640D00D76ADc118E40354159676C62F78" // address _priceFeed
     ]
   });
 }
