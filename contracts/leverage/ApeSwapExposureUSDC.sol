aa5654d0777f3148f3b15ded3a895d501ec662b4
Steven Enamakel <enamakel@mahadao.com>
2022-04-05 20:15:29 +0100

    updated ellipsis router

---
+++ contracts/leverage/ApeSwapExposureUSDC.sol
@@ -0,0 +1,347 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {IEllipsisRouter} from "../interfaces/IEllipsisRouter.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Wrapper} from "../interfaces/IERC20Wrapper.sol";
+import {IFlashBorrower} from "../interfaces/IFlashBorrower.sol";
+import {IFlashLoan} from "../interfaces/IFlashLoan.sol";
+import {ILeverageStrategy} from "../interfaces/ILeverageStrategy.sol";
+import {IPriceFeed} from "../interfaces/IPriceFeed.sol";
+import {ITroveManager} from "../interfaces/ITroveManager.sol";
+import {IUniswapV2Factory} from "../interfaces/IUniswapV2Factory.sol";
+import {IUniswapV2Router02} from "../interfaces/IUniswapV2Router02.sol";
+import {LeverageAccount, LeverageAccountRegistry} from "../account/LeverageAccountRegistry.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {TroveHelpers} from "../helpers/TroveHelpers.sol";
+
+contract ApeSwapExposureUSDC is TroveHelpers, IFlashBorrower, ILeverageStrategy {
+  using SafeMath for uint256;
+
+  address public borrowerOperations;
+
+  ITroveManager public troveManager;
+  IPriceFeed public priceFeed;
+
+  IERC20 public arth;
+  IERC20 public busd;
+  IERC20 public usdc;
+  IERC20 public rewardToken;
+
+  IFlashLoan public flashLoan;
+  LeverageAccountRegistry public accountRegistry;
+
+  IERC20 public lp;
+
+  IERC20Wrapper public arthUsd;
+  IERC20Wrapper public stakingWrapper;
+
+  IEllipsisRouter public ellipsis;
+  IUniswapV2Router02 public apeswapRouter;
+  IUniswapV2Factory public apeswapFactory;
+
+  address private me;
+
+  constructor(
+    address _flashloan,
+    address _arth,
+    address _usdc,
+    address _busd,
+    address _rewardToken,
+    address _ellipsisRouter,
+    address _arthUsd,
+    address _uniswapRouter
+  ) {
+    ellipsis = IEllipsisRouter(_ellipsisRouter);
+
+    busd = IERC20(_busd);
+    arth = IERC20(_arth);
+    usdc = IERC20(_usdc);
+    rewardToken = IERC20(_rewardToken);
+    flashLoan = IFlashLoan(_flashloan);
+    arthUsd = IERC20Wrapper(_arthUsd);
+
+    me = address(this);
+
+    apeswapRouter = IUniswapV2Router02(_uniswapRouter);
+    apeswapFactory = IUniswapV2Factory(apeswapRouter.factory());
+    lp = IERC20(apeswapFactory.getPair(_usdc, _busd));
+  }
+
+  function init(
+    address _borrowerOperations,
+    address _troveManager,
+    address _priceFeed,
+    address _stakingWrapper,
+    address _accountRegistry
+  ) public {
+    require(borrowerOperations == address(0), "already initialized");
+    borrowerOperations = _borrowerOperations;
+    troveManager = ITroveManager(_troveManager);
+    priceFeed = IPriceFeed(_priceFeed);
+    stakingWrapper = IERC20Wrapper(_stakingWrapper);
+    accountRegistry = LeverageAccountRegistry(_accountRegistry);
+  }
+
+  function getAccount(address who) public view returns (LeverageAccount) {
+    return accountRegistry.accounts(who);
+  }
+
+  function openPosition(
+    uint256[] memory finalExposure,
+    uint256[] memory principalCollateral,
+    uint256 minExpectedCollateralRatio,
+    uint256 maxBorrowingFee
+  ) external override {
+    // take the principal
+    busd.transferFrom(msg.sender, address(this), principalCollateral[0]);
+
+    // todo swap excess
+
+    // estimate how much we should flashloan based on how much we want to borrow
+    uint256 flashloanAmount = estimateAmountToFlashloanBuy(finalExposure, principalCollateral);
+
+    bytes memory flashloanData = abi.encode(
+      msg.sender,
+      uint256(0), // action = 0 -> open loan
+      minExpectedCollateralRatio,
+      maxBorrowingFee,
+      finalExposure,
+      principalCollateral
+    );
+
+    flashLoan.flashLoan(address(this), flashloanAmount, flashloanData);
+    _flush(msg.sender);
+  }
+
+  function closePosition(uint256[] memory minExpectedCollateral) external override {
+    bytes memory flashloanData = abi.encode(
+      msg.sender,
+      uint256(1), // action = 0 -> close loan
+      uint256(0),
+      uint256(0),
+      minExpectedCollateral,
+      minExpectedCollateral
+    );
+
+    // todo need to make this MEV resistant
+    uint256 flashloanAmount = troveManager.getTroveDebt(address(getAccount(msg.sender)));
+    flashLoan.flashLoan(address(this), flashloanAmount, flashloanData);
+    _flush(msg.sender);
+  }
+
+  function onFlashLoan(
+    address initiator,
+    uint256 flashloanAmount,
+    uint256 fee,
+    bytes calldata data
+  ) external override returns (bytes32) {
+    require(msg.sender == address(flashLoan), "untrusted lender");
+    require(initiator == address(this), "not contract");
+
+    // decode the data
+    (
+      address who,
+      uint256 action,
+      uint256 minExpectedCollateralRatio,
+      uint256 maxBorrowingFee,
+      uint256[] memory finalExposure,
+      uint256[] memory minCollateralOrPrincipalCollateral
+    ) = abi.decode(data, (address, uint256, uint256, uint256, uint256[], uint256[]));
+
+    // open or close the loan position
+    if (action == 0) {
+      _onFlashloanOpenPosition(
+        who,
+        flashloanAmount,
+        finalExposure,
+        minCollateralOrPrincipalCollateral,
+        minExpectedCollateralRatio,
+        maxBorrowingFee
+      );
+    } else _onFlashloanClosePosition(who, flashloanAmount, minCollateralOrPrincipalCollateral);
+
+    return keccak256("FlashMinter.onFlashLoan");
+  }
+
+  function _onFlashloanOpenPosition(
+    address who,
+    uint256 flashloanAmount,
+    uint256[] memory finalExposure,
+    uint256[] memory principalCollateral,
+    uint256 minExpectedCollateralRatio,
+    uint256 maxBorrowingFee
+  ) internal {
+    LeverageAccount acct = getAccount(who);
+
+    // 1: sell arth for collateral
+    arth.approve(address(ellipsis), flashloanAmount);
+    ellipsis.sellARTHForExact(
+      flashloanAmount,
+      finalExposure[0].sub(principalCollateral[0]), // amountBUSDOut,
+      finalExposure[1], // amountUSDCOut,
+      0, // amountUSDTOut,
+      me,
+      block.timestamp
+    );
+
+    // 2. LP all the collateral
+    usdc.approve(address(apeswapRouter), usdc.balanceOf(me));
+    busd.approve(address(apeswapRouter), busd.balanceOf(me));
+
+    apeswapRouter.addLiquidity(
+      address(usdc),
+      address(busd),
+      usdc.balanceOf(me),
+      busd.balanceOf(me),
+      0,
+      0,
+      me,
+      block.timestamp
+    );
+
+    // 3. Stake and tokenize
+    uint256 collateralAmount = lp.balanceOf(me);
+    lp.approve(address(stakingWrapper), collateralAmount);
+    stakingWrapper.deposit(collateralAmount);
+
+    // 4: send the collateral to the leverage account
+    stakingWrapper.transfer(address(acct), collateralAmount);
+
+    // 5: open loan using the collateral
+    uint256 debt = flashloanAmount.sub(arth.balanceOf(me));
+    openLoan(
+      acct,
+      borrowerOperations,
+      maxBorrowingFee, // borrowing fee
+      debt, // debt
+      collateralAmount, // collateral
+      address(0), // upperHint,
+      address(0), // lowerHint,
+      address(0), // frontEndTag,
+      arth,
+      stakingWrapper
+    );
+
+    // 6. check if we met the min leverage conditions
+    require(_getTroveCR(address(acct)) >= minExpectedCollateralRatio, "min cr not met");
+
+    // 7. payback the loan..
+    arth.approve(address(flashLoan), flashloanAmount);
+    require(arth.balanceOf(me) >= flashloanAmount, "not enough arth for flashloan");
+  }
+
+  function _onFlashloanClosePosition(
+    address who,
+    uint256 flashloanAmount,
+    uint256[] memory minCollateral
+  ) internal {
+    LeverageAccount acct = getAccount(who);
+
+    // 1. send the flashloaned arth to the account
+    arth.transfer(address(acct), flashloanAmount);
+
+    // 2. use the flashloan'd ARTH to payback the debt and close the loan
+    closeLoan(acct, address(0), borrowerOperations, flashloanAmount, arth, stakingWrapper);
+
+    // 3. get the collateral and swap back to arth to back the loan
+    // 4. unstake and un-tokenize
+    uint256 collateralAmount = stakingWrapper.balanceOf(me);
+    stakingWrapper.withdraw(collateralAmount);
+
+    // 5. remove from LP
+    lp.approve(address(apeswapRouter), lp.balanceOf(me));
+    apeswapRouter.removeLiquidity(
+      address(usdc),
+      address(busd),
+      lp.balanceOf(me),
+      0, // amountAMin
+      0, // amountBMin
+      me,
+      block.timestamp
+    );
+
+    busd.approve(address(ellipsis), busd.balanceOf(me));
+    usdc.approve(address(ellipsis), usdc.balanceOf(me));
+
+    ellipsis.buyARTHForExact(
+      busd.balanceOf(me).sub(minCollateral[0]),
+      usdc.balanceOf(me),
+      0,
+      flashloanAmount,
+      me,
+      block.timestamp
+    );
+
+    require(busd.balanceOf(me) >= minCollateral[0], "not enough busd");
+    // require(usdc.balanceOf(me) >= minCollateral[1], "not enough usdc");
+
+    // 4. payback the loan..
+    arth.approve(address(flashLoan), flashloanAmount);
+    require(arth.balanceOf(me) >= flashloanAmount, "not enough for flashload");
+  }
+
+  function estimateARTHReturnedFromClose(address who) public view returns (uint256[3] memory) {
+    uint256 flashloanAmount = troveManager.getTroveDebt(address(getAccount(who)));
+    uint256 collatAmount = troveManager.getTroveColl(address(getAccount(who)));
+
+    uint256 percInLP = collatAmount.mul(1e10).div(lp.totalSupply());
+
+    uint256 usdcInCollat = usdc.balanceOf(address(lp)).mul(percInLP).div(1e10);
+    uint256 busdInCollat = busd.balanceOf(address(lp)).mul(percInLP).div(1e10);
+
+    uint256 arthToBuy = ellipsis.estimateARTHtoBuy(busdInCollat, usdcInCollat, 0);
+
+    return [usdcInCollat, busdInCollat, arthToBuy.sub(flashloanAmount)];
+  }
+
+  function estimateAmountToFlashloanBuy(
+    uint256[] memory finalExposure,
+    uint256[] memory principalCollateral
+  ) public view returns (uint256) {
+    return
+      ellipsis
+      // we have some busd
+        .estimateARTHtoBuy(finalExposure[0].sub(principalCollateral[0]), finalExposure[1], 0)
+        .mul(101)
+        .div(100); // 1% slippage
+  }
+
+  // function estimateCollateralReturned(address who) public view returns (uint256[2] memory) {
+  //   uint256 troveCollateral = troveManager.getTroveColl(address(getAccount(who)));
+  //   uint256 collateralRatio = _getTroveCR(address(getAccount(who)));
+  //   // uint256 troveCollateralDecimals = 18;
+
+  //   // uint256 leverage = ; // say cr = 190%; leverage = 2.1x in 1e18
+  //   uint256 principalCollateral = troveCollateral.div(
+  //     collateralRatio.mul(1e18).div(collateralRatio.sub(100e18))
+  //   );
+
+  //   // estimate amount of usdc and busd in principalCollateral
+  //   uint256 percentage = lp.totalSupply().mul(1e18).div(principalCollateral);
+
+  //   return [
+  //     busd.balanceOf(address(lp)).mul(percentage).div(1e18),
+  //     usdc.balanceOf(address(lp)).mul(percentage).div(1e18)
+  //   ];
+  // }
+
+  function _getTroveCR(address who) internal view returns (uint256) {
+    uint256 price = priceFeed.fetchPrice();
+    uint256 debt = troveManager.getTroveDebt(who);
+    uint256 coll = troveManager.getTroveColl(who);
+    return coll.mul(price).div(debt);
+  }
+
+  function _flush(address to) internal {
+    if (arth.balanceOf(me) > 0) {
+      arth.approve(address(arthUsd), arth.balanceOf(me));
+      arthUsd.deposit(arth.balanceOf(me));
+    }
+    if (arthUsd.balanceOf(me) > 0) arthUsd.transfer(to, arthUsd.balanceOf(me));
+    if (usdc.balanceOf(me) > 0) usdc.transfer(to, usdc.balanceOf(me));
+    if (busd.balanceOf(me) > 0) busd.transfer(to, busd.balanceOf(me));
+    if (rewardToken.balanceOf(me) > 0) rewardToken.transfer(to, rewardToken.balanceOf(me));
+  }
+}
