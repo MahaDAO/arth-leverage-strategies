07498b765817f70e1d65ef28c6121a5ede2f7f36
Steven Enamakel <enamakel@mahadao.com>
2022-04-15 00:44:06 +0100

    removed console.log messages

--- contracts/oracles/TWAPOracle.sol
+++ contracts/oracles/TWAPOracle.sol
@@ -6,7 +6,6 @@ import {Epoch} from "../utils/Epoch.sol";
 import {IPriceFeed} from "../interfaces/IPriceFeed.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import "hardhat/console.sol";
 
 contract TWAPOracle is Epoch, IPriceFeed {
   using SafeMath for uint256;
@@ -14,8 +13,8 @@ contract TWAPOracle is Epoch, IPriceFeed {
   IPriceFeed public oracle;
   uint256 public lastPriceIndex;
   mapping(uint256 => uint256) public priceHistory;
-  uint256 precision = 1e9;
-  uint256 maxPriceChange;
+  uint256 public precision = 1e9;
+  uint256 public maxPriceChange;
 
   constructor(
     address _oracle,
@@ -43,9 +42,7 @@ contract TWAPOracle is Epoch, IPriceFeed {
     uint256 maxPrice = Math.max(priceHistory[lastPriceIndex], priceHistory[lastPriceIndex - 1]);
 
     uint256 priceChange = maxPrice.sub(minPrice).mul(precision).mul(100).div(maxPrice);
-    // console.log("price change", priceChange);
-    // console.log("maxPriceChange", maxPriceChange);
-    // console.log("precision", precision);
+
     require(priceChange < maxPriceChange, "too much price deviation");
   }
 
@@ -54,10 +51,7 @@ contract TWAPOracle is Epoch, IPriceFeed {
 
     uint256 priceTotal = 0;
     for (uint256 index = lastPriceIndex; index > lastPriceIndex - 3; index--) {
-      // console.log("index", index);
-      // console.log("priceTotal before", priceTotal);
       priceTotal = priceTotal.add(priceHistory[index]);
-      // console.log("priceTotal after", priceTotal);
     }
 
     return priceTotal.div(3); // average it out!
