8db46b39ea9f9fc125a117611175b6bae17dd989
0xya <yashagrawal2829@gmail.com>
2022-04-26 14:33:25 +0530

    feat: minor fixes

--- scripts/router/deployEllipsisSwapRouter.ts
+++ scripts/router/deployEllipsisSwapRouter.ts
@@ -3,14 +3,14 @@ import { wait } from "../utils";
 
 async function main() {
   const constructorArguments = [
-    "0x5ab5c56b9db92ba45a0b46a207286cd83c15c939", // address _ellipsisSwap,
-    "0xdde5fdb48b2ec6bc26bb4487f8e3a4eb99b3d633", // address _lp,
-    "0xdde5fdb48b2ec6bc26bb4487f8e3a4eb99b3d633", // address _pool,
-    "0xe52509181feb30eb4979e29ec70d50fd5c44d590", // address _arth,
-    "0x84f168e646d31f6c33fdbf284d9037f59603aa28", // address _arthUsd,
-    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // address _usdc,
-    "0xc2132d05d31c914a87c6611c10748aeb04b58e8f", // address _usdt,
-    "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063" // address _busd
+    "0x5035cc9988f88f99cdef210d833957a80236c0a4", // address _zap,
+    "0x4CfAaBd5920021359BB22bB6924CCe708773b6AC", // address _lp,
+    "0x1d4B4796853aEDA5Ab457644a18B703b6bA8b4aB", // address _pool,
+    "0xB69A424Df8C737a122D0e60695382B3Eec07fF4B", // address _arth,
+    "0x88fd584dF3f97c64843CD474bDC6F78e398394f4", // address _arthUsd,
+    "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d", // address _usdc,
+    "0x55d398326f99059fF775485246999027B3197955", // address _usdt,
+    "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56" // address _busd
   ];
 
   // We get the contract to deploy
