77788c7619ba6daa9bdc69b6567e003129bd5484
0xya <yashagrawal2829@gmail.com>
2022-04-26 12:21:43 +0530

    feat: revert router to older version

--- scripts/router/test-ellipsis.ts
+++ scripts/router/test-ellipsis.ts
@@ -6,7 +6,7 @@ async function main() {
   // We get the contract to deploy
   const instance = await ethers.getContractAt(
     "EllipsisARTHRouter",
-    "0xD419461fec33fb758E3f518D12412d9ade0AB2c0"
+    "0x27FB00e7B8d20190A686E55e2f67EF8fACB911Da"
   );
 
   // await approve(
@@ -15,11 +15,11 @@ async function main() {
   //   instance.address
   // );
 
-  // await approve(
-  //   "0xe9e7cea3dedca5984780bafc599bd69add087d56", // busd
-  //   "3000000000000000000000000",
-  //   instance.address
-  // );
+  await approve(
+    "0xe9e7cea3dedca5984780bafc599bd69add087d56", // busd
+    "3000000000000000000000000",
+    instance.address
+  );
 
   // await approve(
   //   "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // usdc
@@ -41,16 +41,16 @@ async function main() {
 
   console.log("i am", owner.address);
 
-  console.log("swapping 2 arth for 1 usdt and 3 busd");
-  const tx = await instance.sellARTHForExact(
-    "20000000000000000", // uint256 amountArthInMax,
-    "10000000000000000", // uint256 amountBUSDOut,
-    "10000000000000000", // uint256 amountUSDCOut,
-    "0", // uint256 amountUSDTOut,
-    owner.address, // address to,
-    Math.floor(Date.now() / 1000) + 3600 // uint256 deadline
-  );
-  console.log("sell", tx.hash);
+  // console.log("swapping 2 arth for 1 usdt and 3 busd");
+  // const tx = await instance.sellARTHForExact(
+  //   "20000000000000000", // uint256 amountArthInMax,
+  //   "10000000000000000", // uint256 amountBUSDOut,
+  //   "10000000000000000", // uint256 amountUSDCOut,
+  //   "0", // uint256 amountUSDTOut,
+  //   owner.address, // address to,
+  //   Math.floor(Date.now() / 1000) + 3600 // uint256 deadline
+  // );
+  // console.log("sell", tx.hash);
 
   // console.log("swapping 1 busd for 1 arth.usd");
   // const tx2 = await instance.buyARTHForExact(
@@ -62,6 +62,17 @@ async function main() {
   //   Math.floor(Date.now() / 1000) + 3600 // uint256 deadline
   // );
   // console.log("buy", tx2.hash);
+
+  const tx3 = await instance.sellTokenForToken(
+    "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+    0,
+    1,
+    "1000000000000000000",
+    "0",
+    owner.address,
+    Math.floor((Date.now() + 5 * 60 * 60 * 1000) / 1000)
+  );
+  console.log(tx3.hash);
 }
 
 // eslint-disable-next-line no-unused-vars
