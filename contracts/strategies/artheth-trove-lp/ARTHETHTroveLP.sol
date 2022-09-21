906b678013baffe58e0f0c22545bb2cacd570c71
Yash Agrawal <yashagrawal2299@gmail.com>
2022-09-21 13:46:27 +0530

    feat: update logic of deposit

--- contracts/strategies/artheth-trove-lp/ARTHETHTroveLP.sol
+++ contracts/strategies/artheth-trove-lp/ARTHETHTroveLP.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import {IERC721, IERC721Metadata, ERC721, ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -11,7 +13,8 @@ import {IERC721, IERC721Metadata, ERC721, ERC721Enumerable} from "@openzeppelin/
 import {IBorrowerOperations} from "../../interfaces/IBorrowerOperations.sol";
 import {INonfungiblePositionManager} from "../../interfaces/INonfungiblePositionManager.sol";
 
-contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Pausable {
+contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Pausable, ReentrancyGuard {
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
 
     event Deposit(address indexed dst, uint256 wad, uint256 tokenId);
@@ -27,16 +30,16 @@ contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Paus
         uint256 amount1;
     }
 
-    IERC20 public arth;
-    IERC20 public weth;
     uint24 public fee;
-
     Counters.Counter private _tokenIdTracker;
     mapping(uint256 => Position) public positions;
+
+    IERC20 public arth;
+    IERC20 public weth;
     IBorrowerOperations public borrowerOperations;
     INonfungiblePositionManager public uniswapNFTManager;
 
-    // todo; need to handle the scenario when the trove gets liquidated?
+    // TODO: the scenario when the trove gets liquidated?
 
     constructor(
         address _borrowerOperations,
@@ -44,10 +47,12 @@ contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Paus
         address _arth,
         address _weth,
         uint24 _fee
-    ) ERC721("ARTH/ETH LP Strategy", "ARTHETH-lp") {
+    ) 
+        ERC721("ARTH/ETH LP Strategy", "ARTHETH-lp") 
+    {
+        fee = _fee;
         arth = IERC20(_arth);
         weth = IERC20(_weth);
-        fee = _fee;
         borrowerOperations = IBorrowerOperations(_borrowerOperations);
         uniswapNFTManager = INonfungiblePositionManager(_uniswapNFTManager);
 
@@ -67,12 +72,20 @@ contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Paus
             super.supportsInterface(interfaceId);
     }
 
-    function openTrove() external payable onlyOwner {
-        // used by the fund manager to open the trove
+    /// @notice Open trove, and keep the ARTH minted in the contract itself.
+    function openTrove() external payable onlyOwner nonReentrant {
+        // borrowerOperations.openTrove{value: msg.value}(
+        //     _maxFee, 
+        //     _ARTHAmount, 
+        //     _upperHint, 
+        //     _lowerHint, 
+        //     _frontEndTag
+        // );
     }
 
-    function closeTrove() external payable onlyOwner {
-        // used by the fund manager to close the trove
+    /// @notice Closes the trove, and keep the ETH returned in the contract itself.
+    function closeTrove() external payable onlyOwner nonReentrant {
+        borrowerOperations.closeTrove();
     }
 
     function deposit(
@@ -82,12 +95,24 @@ contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Paus
         address upperHint,
         address lowerHint,
         INonfungiblePositionManager.MintParams memory mintParams
-    ) public payable {
-        // We cannot just use balanceOf to create the new tokenId because tokens
-        // can be burned (destroyed), so we need a separate counter.
+    ) 
+        public 
+        payable 
+        nonReentrant 
+    {
+        address _me = address(this);
+
+        // Check that we are receiving appropriate amount of ETH and 
+        // Mint the new strategy NFT.
+        require(
+            msg.value == mintParams.amount1Desired.add(ethToLock), 
+            "Invalid ETH amount"
+        );
+        _tokenIdTracker.increment(); // Counters start from 0, hence initial increment is required.
         _mint(msg.sender, _tokenIdTracker.current());
 
-        // 1. mint ARTH
+        // 2. Mint ARTH and track ARTH balance changes due to this current tx.
+        uint256 arthBeforeMinting = arth.balanceOf(_me);
         borrowerOperations.adjustTrove{value: ethToLock}(
             maxFee,
             arthToMint,
@@ -96,15 +121,28 @@ contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Paus
             upperHint,
             lowerHint
         );
+        uint256 arthAfterMinting = arth.balanceOf(_me);
 
-        // 2. LP the ARTH/ETH
-        mintParams.recipient = address(this);
+        // Check that appropriate amount of ARTH was minted.
+        require(
+            arthAfterMinting.sub(arthBeforeMinting) >= mintParams.amount0Desired, 
+            "Not enough ARTH"
+        );
+
+        // 3. LP the ARTH/ETH.
+        mintParams.fee = fee;
+        mintParams.recipient = _me;
         mintParams.token0 = address(arth);
         mintParams.token1 = address(weth);
-        (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1) = uniswapNFTManager
-            .mint(mintParams);
+        mintParams.deadline = block.timestamp;
+        (
+            uint256 tokenId, 
+            uint128 liquidity, 
+            uint256 amount0, 
+            uint256 amount1
+        ) = uniswapNFTManager.mint{value: mintParams.amount1Desired}(mintParams);
 
-        // record the position
+        // 4. Record the position.
         positions[_tokenIdTracker.current()] = Position({
             eth: msg.value,
             coll: ethToLock,
@@ -114,9 +152,10 @@ contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Paus
             amount0: amount0,
             amount1: amount1
         });
+        
+        // 5. Shouldn't we refund any dust ETH/WETH or ARTH that might be left after new LP position?
 
         emit Deposit(msg.sender, msg.value, _tokenIdTracker.current());
-        _tokenIdTracker.increment();
     }
 
     function withdraw(
@@ -126,20 +165,31 @@ contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Paus
         address lowerHint,
         INonfungiblePositionManager.DecreaseLiquidityParams memory decreaseLiquidityParams,
         INonfungiblePositionManager.CollectParams memory collectParams
-    ) public {
-        require(ERC721.ownerOf(tokenId) == msg.sender, "not owner");
-        _burn(tokenId);
+    )
+        public 
+        payable 
+        nonReentrant 
+    {
+        require(ERC721.ownerOf(tokenId) == msg.sender, "Not owner");
 
+        // 1. Burn the strategy NFT and fetch position details.
+        _burn(tokenId);
         Position memory position = positions[tokenId];
 
-        // trigger the withdrawal
-        // 2. Remove the LP for ARTH/ETH
+        // 2. Claim the fees.
+        _collectFees(position, collectParams);
+
+        // 3. Remove the LP for ARTH/ETH.
+        decreaseLiquidityParams.deadline = block.timestamp;
+        decreaseLiquidityParams.liquidity = position.liquidity;
         decreaseLiquidityParams.tokenId = position.uniswapNftId;
         (uint256 amount0, uint256 amount1) = uniswapNFTManager.decreaseLiquidity(
             decreaseLiquidityParams
         );
-
-        // todo: if we have less ARTH then we swap the ETH for ARTH in the ARTH/ETH pool
+        
+        if (amount0 < position.debt) {
+            // TODO: if we have less ARTH then we swap the ETH for ARTH in the ARTH/ETH pool
+        }
 
         borrowerOperations.adjustTrove(
             maxFee,
@@ -152,18 +202,41 @@ contract ARTHETHTroveLP is Ownable, ERC721Enumerable, ERC721Burnable, ERC721Paus
 
         // todo: if we have more ARTH then we swap the ARTH for ETH in the ARTH/ETH pool
 
-        // send the ether back
-        (bool success, ) = msg.sender.call{value: position.eth}("");
-        require(success, "withdraw failed");
+        // Send the ether back.
+        (bool success, /* bytes data */) = msg.sender.call{value: position.eth}("");
+        require(success, "Withdraw failed");
 
         emit Withdrawal(msg.sender, position.eth, tokenId);
+    }
+
+    function _collectFees(
+        Position memory position,
+        INonfungiblePositionManager.CollectParams memory collectParams
+    )
+        internal
+    {
+        // TODO: add MAHA rewards as well.
+
+        // Form the fees collection params. 
+        // Fees will directly be sent to the owner.
+        collectParams.recipient = msg.sender;
+        collectParams.amount0Max = type(uint128).max;
+        collectParams.amount1Max = type(uint128).max;
+        collectParams.tokenId = position.uniswapNftId;
+
+        // Trigger the fee collection for the nft owner.
+        uniswapNFTManager.collect(collectParams);
     }
 
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 tokenId
-    ) internal virtual override(ERC721, ERC721Enumerable, ERC721Pausable) {
+    ) 
+        internal 
+        virtual 
+        override(ERC721, ERC721Enumerable, ERC721Pausable) 
+    {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 }
