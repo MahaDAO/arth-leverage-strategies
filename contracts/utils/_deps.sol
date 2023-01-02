f14faa19df290abc5a20f954e7a7596b0f1d8787
Steven Enamakel <enamakel@mahadao.com>
2023-01-02 05:44:45 +0530

    added a dummy dependency contract

---
+++ contracts/utils/_deps.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+contract Deps {}
